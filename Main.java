8685521835a9acdb43c19f1822a36d483500caf6
Vamshi Goud <vgoud55@gmail.com>
2019-10-17 17:11:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello!");
+		int x[] = new int[6];
+		for (int i = 0;i <= 5;i++) 
+		{
+			x[i] = i;
+		}
+		for (int j = 0;j <= 5;j++) 
+		{
+			System.out.println(x[j]);	
+		}
+	}
+}
+
